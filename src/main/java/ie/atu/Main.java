892a709f52c1c1ec28b2e2cacf895fe3ba47c705
NathanmcCormack <G00414780@atu.ie>
2023-12-10 18:14:35 +0000

    Created two objects in main

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,17 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car car1 = new Car("car", "black", 4, "BMW E30", 35000);
+        Bike bike1 = new Bike();
+
+        bike1.setElectric(true);
+        bike1.setPrice(2000);
+        bike1.setColour("Black");
+        bike1.setVehicle("bike");
+        bike1.setNumWheels(2);
+
+        System.out.println(car1.toString());
+        System.out.println(bike1.toString());
     }
 }
